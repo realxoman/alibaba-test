28edb393e2f19dc35892d318df3e0df5185cfedb
Alixo <fromiran98@gmail.com>
2021-07-16 15:00:11 +0430

    v1.5

--- pages/index.vue
+++ pages/index.vue
@@ -3,27 +3,23 @@
     <div class="d-flex-column">
       <div :class="[$style.searchForm,'d-flex']">
         <form @submit.prevent>
-        <div :class="$style.searchInput">
-          <font-awesome-icon :class="$style.icon" icon="search" />
-         <input
-            v-model="search"
-            type="text"
-            v-on:keyup="filterByName($event)"
-            @focus="search = ''"
-            placeholder="Search for a country..."
-          />
-        </div></form>
+          <div :class="$style.searchInput">
+            <font-awesome-icon :class="$style.icon" icon="search" />
+            <input v-model="search" type="text" v-on:keyup="filterByName($event)" @focus="search = ''"
+              placeholder="Search for a country..." />
+          </div>
+        </form>
         <select :class="$style.selection" v-on:change="onChange($event)" :selected="selectedReg">
-        <option value="none">Filter by Region</option>
-        <option v-for="region in regions" :key="region" :value="region">{{region}}</option>
-      </select>
-      
+          <option value="none">Filter by Region</option>
+          <option v-for="region in regions" :key="region" :value="region">{{region}}</option>
+        </select>
+
       </div>
-      
-    
+
+
       <div v-if="countriesList" :class="[$style.row, 'd-flex', 'wrap']">
         <div v-for="country in filtered" :class="$style.cardWrapper">
-          <Countries  :country="country" :key="country.name" />
+          <Countries :country="country" :key="country.name" />
         </div>
       </div>
     </div>
@@ -50,49 +46,61 @@
       };
     },
     async created() {
-    this.countries = await this.res;
-    this.getRegions();
-    this.filtered = this.countries;
-  },
+      this.countries = await this.res;
+      this.getRegions();
+      this.filtered = this.countries;
+    },
     computed: {
       countriesList() {
         return this.countries || false
       },
-      
-  },methods: {
-        getRegions() {
-          const regions = this.countries.map(c => c.region);
-          let arr = [];
-          regions.forEach(el => {
-            return arr.push(el);
-          });
-          const regionsList = arr.filter((item, pos) => {
-            return arr.indexOf(item) == pos && item != "";
-          });
-          this.regions = regionsList;
-        },
-        onChange(e) {
+
+    },
+    methods: {
+      getRegions() {
+        const regions = this.countries.map(c => c.region);
+        let arr = [];
+        regions.forEach(el => {
+          return arr.push(el);
+        });
+        const regionsList = arr.filter((item, pos) => {
+          return arr.indexOf(item) == pos && item != "";
+        });
+        this.regions = regionsList;
+      },
+      onChange(e) {
+        if (e.target.value == "none") {
+          this.filtered = this.countries
+          this.search = "";
+        } else {
           this.search = e.target.value;
+
           if (this.selectedReg !== e.target.value) {
+
+
             const filteredList = this.countries.filter(
               item => item.region === e.target.value
             );
             this.filtered = filteredList;
             this.selectedReg = e.target.value;
+
           }
-        },
-        focusInput() {
-          this.search = "";
-        },
-        filterByName(e) {
-          const val = e.target.value.toLowerCase();
-          const filteredName = this.countries.filter(c => {
-            return c.name.toLowerCase().includes(val);
-          });
-          this.filtered = filteredName;
         }
+
+      },
+      focusInput() {
+        this.search = "";
+      },
+      filterByName(e) {
+
+        const val = e.target.value.toLowerCase();
+        const filteredName = this.countries.filter(c => {
+          return c.name.toLowerCase().includes(val);
+        });
+        this.filtered = filteredName;
       }
     }
+  }
 
 </script>
 
